b22fede7a71e761b28659f497721c5f5303b3ae5
Matthieu Béteille <matthieu.beteille@gmail.com>
2015-11-22 17:47:16 +0000

    tickets: improve ui

--- src/cljs/pancetta/components/tickets.cljs
+++ src/cljs/pancetta/components/tickets.cljs
@@ -5,7 +5,7 @@
 
 (def style {:item {:background-color (:bg-widget ui/colors)
                    :border-radius 3
-                   :margin-bottom 20
+                   :margin-bottom 2
                    :padding 20}
             :cross {:float "right"
                     :cursor "pointer"
@@ -31,4 +31,6 @@
     (fn []
       (if (nil? @tickets)
         [:div "You have no tickets"]
-        [:div (map #(-> [:div {:key (get % 0)} [ticket-component state %]]) @tickets)]))))
+        [:div
+          [:h2 "My tickets"]
+          (map #(-> [:div {:key (get % 0)} [ticket-component state %]]) @tickets)]))))
